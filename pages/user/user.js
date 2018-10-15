4015b836e954a7aa13ef94aaa3014fb3040459dd
Walkerontheway <1521481790@qq.com>
2018-10-15 16:26:41 +0800

    个人资料

---
+++ pages/user/user.js
@@ -0,0 +1,97 @@
+// pages/user/user.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 安全退出
+   */
+  logout: function() {
+    wx.redirectTo({
+      url: '../login/login',
+    })
+  },
+
+  /**
+   * 我的消息
+   */
+  mynews: function() {
+
+  },
+
+  /**
+   * 我的银行卡
+   */
+  mycards: function() {
+
+  },
+
+  /**
+   * 更新日志
+   */
+  updatelog: function() {
+
+  },
+
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
