0eb3947f0ad0d1344de1c3c2fef7e44b3cbc5b72
Walkerontheway <1521481790@qq.com>
2018-10-16 15:12:01 +0800

    我的银行卡

--- pages/bankcard/bankcard.js
+++ pages/bankcard/bankcard.js
@@ -1,18 +1,57 @@
 // pages/bankcard/bankcard.js
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    list:[],
+    ishide:true
   },
+
+/**
+ * 请求后台数据
+ */
+  getList: function () {
+    var that = this;
+    var phone = "13770207216";
+    wx.request({
+      url: app.globalData.http_url_head + 'bankCard/query/' + phone,
+      method: 'POST',
+      success: function (res) {
+        if (res.statusCode == 200) {
+          var list = res.data.data;
+          if (list.length < 1){
+            that.setData({
+              ishide: false
+            })
+          }else{
+            that.setData({
+              list: list
+            })
+          }
+        } else {
+          wx.showToast({
+            title: '获取银行卡列表失败',
+            icon: 'none',
+            duration: 2000 //持续的时间
+          })
+        }
+      },
+      fail: function (e) {
+        console.log("请求后台异常！")
+      }
+    })
+  },
+
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var that = this;
+    that.getList();
   },
 
   /**
