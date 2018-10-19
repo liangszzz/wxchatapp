a5d3e4e017aa16c9c2f294e5ff9c925ca86087a4
Walkerontheway <1521481790@qq.com>
2018-10-19 16:50:29 +0800

    我的消息

--- pages/news/news.js
+++ pages/news/news.js
@@ -7,10 +7,14 @@ Page({
    */
   data: {
     list: [],
-    pageNumber:1, //当前第几页
-    pageSize:9 //每页显示数量
+    userInfo: {}, //当前用户的openid
+    pageNumber: 1, //当前第几页
+    pageSize: 9 //每页显示数量
   },
 
+  /**
+   * 请求后台获取数据
+   */
   getList: function() {
     var that = this;
     wx.request({
@@ -19,17 +23,32 @@ Page({
         token: app.globalData.userInfo.token
       },
       data: {
-        phone: "13770207216",
+        open_id: that.data.userInfo.openId,
         page: that.data.pageNumber,
         size: that.data.pageSize
       },
       method: 'POST',
-      success: function (res) {
-        if (res.statusCode == 200) {
+      success: function(res) {
+        console.log(res);
+        if (res.statusCode == 200 && res.data.code == 0) {
           var list = res.data.data;
-          that.setData({
-            list: list
-          })
+          var datalist = that.data.list;
+          if (list.length > 0) {
+            // 回调函数
+            for (var i = 0; i < list.length; i++) {
+              datalist.push(list[i]);
+            }
+            // 设置数据
+            that.setData({
+              list: datalist
+            })
+          } else {
+            wx.showToast({
+              title: '暂无更多数据',
+              icon: 'none',
+              duration: 2000 //持续的时间
+            })
+          }
         } else {
           wx.showToast({
             title: '获取消息列表失败',
@@ -38,7 +57,7 @@ Page({
           })
         }
       },
-      fail: function (e) {
+      fail: function(e) {
         console.log("请求后台异常！")
       }
     })
@@ -48,8 +67,10 @@ Page({
    */
   onLoad: function(options) {
     var that = this;
+    that.setData({
+      userInfo: app.globalData.userInfo
+    })
     that.getList();
-
   },
 
   /**
@@ -85,32 +106,69 @@ Page({
    */
   onPullDownRefresh: function() {
     wx.showNavigationBarLoading() //在标题栏中显示加载
-    //当前页面设置为1，重新请求后台
     var that = this;
+    //当前页面设置为1，重新请求后台
     that.setData({
-      pageNumber:1,
+      pageNumber: 1,
     })
-    that.getList();
-    wx.hideNavigationBarLoading() //在标题栏中显示加载
-    wx.stopPullDownRefresh(); //结束下拉刷新，页面回弹
+
+    setTimeout(function () {
+      wx.request({
+        url: app.globalData.http_url_head + 'message/query',
+        header: {
+          token: app.globalData.userInfo.token
+        },
+        data: {
+          open_id: that.data.userInfo.openId,
+          page: that.data.pageNumber,
+          size: that.data.pageSize
+        },
+        method: 'POST',
+        success: function (res) {
+          console.log(res);
+          if (res.statusCode == 200 && res.data.code == 0) {
+            var list = res.data.data;
+            // 设置数据
+            that.setData({
+              list: list
+            })
+          } else {
+            wx.showToast({
+              title: '获取消息列表失败',
+              icon: 'none',
+              duration: 2000 //持续的时间
+            })
+          }
+        },
+        fail: function (e) {
+          console.log("请求后台异常！")
+        },
+        complete: function () {
+          wx.stopPullDownRefresh();
+          wx.hideNavigationBarLoading();
+        }
+      })
+    }, 1000)
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function() {
-    var that = this;
-    // 显示加载图标
     wx.showLoading({
       title: '玩命加载中',
     })
+    var that = this;
     // 页数+1
     var pageNumber = that.data.pageNumber + 1;
     that.setData({
-      pageNumber : pageNumber
+      pageNumber: pageNumber
     })
-    that.getList();
-    wx.hideLoading();//隐藏加载图标
+    setTimeout(function () {
+      that.getList();
+      wx.hideLoading(); //隐藏加载图标
+    }, 1000)
+    
   },
 
   /**
