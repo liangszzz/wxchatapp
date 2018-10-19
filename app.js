4a080a364ec9a2d9a56305b2c26c2bd34cba23ac
yiyan1992 <liang-shan@outlook.com>
2018-10-19 10:38:27 +0800

    登陆

--- app.js
+++ app.js
@@ -1,79 +1,30 @@
 //app.js
 App({
     onLaunch: function() {
-        // 展示本地存储能力
-        var logs = wx.getStorageSync('logs') || []
-        logs.unshift(Date.now())
-        wx.setStorageSync('logs', logs)
-
-
-        // 获取用户信息
-        wx.getSetting({
+        wx.login({
             success: res => {
-                if (res.authSetting['scope.userInfo']) {
-                    // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-                    wx.getUserInfo({
-                        success: res => {
-                            // 可以将 res 发送给后台 如果没有这个用户
-                            console.log("可以将 res 发送给后台解码出 unionId")
-                            console.log(res)
-                            wx.request({
-                                url:this.globalData.http_url_head + 'login/wxLaunch',
-                                method: "POST",
-                                data: res,
-                                success: res => {
-                                    console.log(res)
-                                },
-                                fail: res => {
-
-                                }
-                            })
-
-                            // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-                            // 所以此处加入 callback 以防止这种情况
-                            if (this.userInfoReadyCallback) {
-                                this.userInfoReadyCallback(res)
-                            }
-                        }
-                    })
-                } else {
-                    //没有授权,跳转到注册页面
-                    wx.redirectTo({
-                        url: '/pages/login/login',
-                        success: function(res) {},
-                        fail: function(res) {}
-                    })
-                }
-            }
-        })
-
-        //检查网络状态
-        wx.getNetworkType({
-            success: res => {
-                console.log(res)
-                if (res.networkType != "wifi") {
-                    console.log("###1")
-                }
+                wx.request({
+                    url: this.globalData.http_url_head + 'login/wxLogin/' + res.code,
+                    method: "POST",
+                    success: res => {
+                        this.globalData.user_info = res.data.entity
+                        wx.reLaunch({
+                            url: '/pages/clbaseinfo/baseInfo'
+                        })
+                    }
+                })
             },
             fail: res => {
+                console.log("login fail")
                 console.log(res)
-            }
-        })
-        //监听网络状态
-        wx.onNetworkStatusChange(function(res) {
-            console.log(res)
-            if (res.networkType != "wifi") {
-                console.log("no wifi")
             }
         })
     },
+
     onShow: function(options) {
-        // Do something when show.
-        console.log("####show")
         console.log(options)
     },
     onHide: function() {
-        // Do something when hide.
         console.log("hide")
     },
     onError: function(msg) {
@@ -82,7 +33,7 @@ App({
     onPageNotFound: function(options) {
         console.log(options)
         wx.redirectTo({
-            url: 'pages/index/index.js'
+            url: 'pages/login/login'
         }) // 如果是 tabbar 页面，请使用 wx.switchTab
     },
     globalData: {
--- pages/login/login.js
+++ pages/login/login.js
@@ -2,105 +2,108 @@
 const app = getApp()
 
 Page({
-  data: {
-    phone: "",
-    phoneCheck: false,
-    invitation_code: "",
-    invitationCheck: false,
-    btnRegistered: true
-  },
-  onLoad: function() {
-
-  },
-  bindPhoneInput: function(e) {
-    this.setData({
-      phone: e.detail.value
-    })
-    if (this.data.phone.length == 11) {
-      this.setData({
-        phoneCheck: true
-      })
-    } else {
-      this.setData({
-        phoneCheck: false
-      })
-    }
-    if (this.data.phoneCheck && this.data.invitationCheck) {
-      this.setData({
-        btnRegistered: false
-      })
-    } else {
-      this.setData({
+    data: {
+        phone: "",
+        phoneCheck: false,
+        invitation_code: "",
+        invitationCheck: false,
         btnRegistered: true
-      })
-    }
-  },
-  bindInvitationInput: function(e) {
-    this.setData({
-      invitation_code: e.detail.value
-    })
-    if (this.data.invitation_code.length > 0) {
-      this.setData({
-        invitationCheck: true
-      })
-    } else {
-      this.setData({
-        invitationCheck: false
-      })
-    }
-    if (this.data.phoneCheck && this.data.invitationCheck) {
-      this.setData({
-        btnRegistered: false
-      })
-    } else {
-      this.setData({
-        btnRegistered: true
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    var data = e.detail.userInfo
-    data.phone = this.data.phone
-    data.invitation_code = this.data.invitation_code
+    },
+    onLoad: function() {
 
-    wx.login({
-      timeout: 50000,
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-        if (res.errMsg == "login:ok") {
-          data.code = res.code
-          wx.request({
-            url: app.globalData.http_url_head + "login/wxRegister",
-            data: data,
-            method: "POST",
-            success: result => {
-              console.log(result)
-              if (result.statusCode == 200 && result.data.code==0) {
-                app.globalData.userInfo=result.data.entity
-                  wx.redirectTo({
-                  url: '../clbaseinfo/baseInfo',
-                  success: res => {
-                    console.log(res)
-                  }
-                })
-              } else {
-                wx.showModal({
-                  title: '注册失败',
-                  content: "手机号或邀请码有误!",
-                  showCancel: false,
-                  confirmText: "确定"
-                })
-              }
-            },
-            fail: e => {
-              console.log(e)
-            }
-          })
+    },
+    bindPhoneInput: function(e) {
+        this.setData({
+            phone: e.detail.value
+        })
+        if (this.data.phone.length == 11) {
+            this.setData({
+                phoneCheck: true
+            })
+        } else {
+            this.setData({
+                phoneCheck: false
+            })
         }
-      },
-      fail: res => {
-        console.log("#登陆失败")
-      }
-    })
-  }
+        if (this.data.phoneCheck && this.data.invitationCheck) {
+            this.setData({
+                btnRegistered: false
+            })
+        } else {
+            this.setData({
+                btnRegistered: true
+            })
+        }
+    },
+    bindInvitationInput: function(e) {
+        this.setData({
+            invitation_code: e.detail.value
+        })
+        if (this.data.invitation_code.length > 0) {
+            this.setData({
+                invitationCheck: true
+            })
+        } else {
+            this.setData({
+                invitationCheck: false
+            })
+        }
+        if (this.data.phoneCheck && this.data.invitationCheck) {
+            this.setData({
+                btnRegistered: false
+            })
+        } else {
+            this.setData({
+                btnRegistered: true
+            })
+        }
+    },
+    getUserInfo: function(e) {
+        var data = e.detail.userInfo
+        data.phone = this.data.phone
+        data.invitation_code = this.data.invitation_code
+
+        wx.login({
+            timeout: 50000,
+            success: res => {
+                // 发送 res.code 到后台换取 openId, sessionKey, unionId
+                if (res.errMsg == "login:ok") {
+                    data.code = res.code
+                    wx.request({
+                        url: app.globalData.http_url_head + "login/wxRegister",
+                        data: data,
+                        method: "POST",
+                        success: result => {
+                            console.log(result)
+                            if (result.statusCode == 200 && result.data.code == 0) {
+                                app.globalData.userInfo = result.data.entity
+                                wx.reLaunch({
+                                    url: '../clbaseinfo/baseInfo',
+                                    success: res => {
+                                        console.log(res)
+                                    },
+                                    fail:res=>{
+                                        console.log(res)
+                                    }
+                                })
+                            } else {
+                                wx.showModal({
+                                    title: '注册失败',
+                                    content: "手机号或邀请码有误!",
+                                    showCancel: false,
+                                    confirmText: "确定"
+                                })
+                            }
+                        },
+                        fail: e => {
+                            console.log(e)
+                        }
+                    })
+                }
+            },
+            fail: res => {
+                console.log("#登陆失败")
+            }
+        })
+    }
 })
