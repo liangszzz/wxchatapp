04e1a6551007e5cfc68c27787df70dd4a961a84b
Walkerontheway <1521481790@qq.com>
2018-10-22 15:52:53 +0800

    协议

---
+++ pages/agreement/agreement.js
@@ -0,0 +1,82 @@
+// pages/agreement/agreement.js
+const app = getApp()
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    src:""
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var type = options.type;
+    //根据不同type 1：借款合同 2：产品说明 3：自动还款协议
+    wx.request({
+      url: app.globalData.http_url_head + 'agreement/query/' + type,
+      header: {
+        token: app.globalData.userInfo.token
+      },
+      method: 'POST',
+      success:function(res){
+        console.log(res);
+      },
+      fail:function(){
+        console.log("请求后台失败");
+      }
+    })
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
