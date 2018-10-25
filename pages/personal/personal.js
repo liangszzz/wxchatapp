06c5901c9a92c87d244c394d3a5e52b34c5e145f
Walkerontheway <1521481790@qq.com>
2018-10-25 14:32:03 +0800

    个人资料

--- pages/personal/personal.js
+++ pages/personal/personal.js
@@ -6,12 +6,13 @@ Page({
    * 页面的初始数据
    */
   data: {
-    biz_order_no:'TEST201803081040',
+    biz_order_no: '',
     userInfo: {}, //当前用户的openid
     check: 0,
-    imglist: ['/assets/images/shenfen1.jpg', '/assets/images/shenfen1.jpg'],
-    carlist: ['/assets/images/baoma1.jpg', '/assets/images/baoma2.jpg', '/assets/images/baoma3.jpg', '/assets/images/baoma4.jpg', '/assets/images/baoma5.jpg', '/assets/images/baoma6.jpg'],
-
+    clCarInfo: {},
+    clUserInfo: {},
+    imglist: [],
+    carlist: [],
   },
 
   /**
@@ -19,7 +20,6 @@ Page({
    */
   previewImage: function(e) {
     var currentUrl = e.target.dataset.src;
-    console.log(currentUrl);
     wx.previewImage({
       current: currentUrl, // 当前显示图片的http链接
       urls: this.data.imglist // 需要预览的图片http链接列表
@@ -31,7 +31,6 @@ Page({
    */
   previewImageCar: function(e) {
     var currentUrl = e.target.dataset.src;
-    console.log(currentUrl);
     wx.previewImage({
       current: currentUrl, // 当前显示图片的http链接
       urls: this.data.carlist // 需要预览的图片http链接列表
@@ -43,7 +42,7 @@ Page({
    */
   contract: function() {
     wx.navigateTo({
-      url:'../agreement/agreement?type=1',
+      url: '../agreement/agreement?type=1',
     })
   },
 
@@ -77,39 +76,41 @@ Page({
   formSubmit: function(e) {
     var check = this.data.check;
     var openId = this.data.userInfo.openId;
+    var biz_order_no = this.data.biz_order_no;
+    var formId = e.detail.formId;
     if (check == 0) {
       wx.showToast({
         title: '请先阅读并理解《借款合同》及《产品说明》',
-        icon:"none",
-        duration:2000
+        icon: "none",
+        duration: 2000
       })
     } else {
       //请求后台，修改订单状态及插入formId
-     wx.request({
-       url: app.globalData.http_url_head + "baseInfo/update",
-       header: {
-         token: app.globalData.userInfo.token
-       },
-       data:{
-         formId: e.detail.formId,
-         openId: openId,
-         biz_order_no: this.data.biz_order_no
-       },
-       method: 'POST',
-       success:function(res){
-        if(res.statusCode == 200 && res.data.code== 0){
-          wx.navigateTo({
-            url: '../auditLenders/auditLenders',
-          })
+      wx.request({
+        url: app.globalData.http_url_head + "baseInfo/update",
+        header: {
+          token: app.globalData.userInfo.token
+        },
+        data: {
+          biz_order_no: biz_order_no,
+          openId: openId,
+          formId: formId
+        },
+        method: 'POST',
+        success: function(res) {
+          if (res.statusCode == 200 && res.data.code == 0) {
+            wx.navigateTo({
+              url: '../auditLenders/auditLenders?biz_order_no=' + biz_order_no,
+            })
+          }
+        },
+        fail: function() {
+          console.log("获取后台失败！");
         }
-       },
-       fail:function(){
-         console.log("获取后台失败！");
-       }
-     })
+      })
     }
 
-    
+
   },
 
   /**
@@ -117,9 +118,45 @@ Page({
    */
   onLoad: function(options) {
     var that = this;
-    that.setData({
-      userInfo: app.globalData.userInfo
-    })  
+    var bizOrderNo = options.bizOrderNo;
+    var carlist = that.data.carlist;
+    var imglist = that.data.imglist;
+    //请求后台获取相关信息
+    wx.request({
+      url: app.globalData.http_url_head + "baseInfo/query",
+      header: {
+        token: app.globalData.userInfo.token
+      },
+      data: {
+        biz_order_no: bizOrderNo
+      },
+      method: 'POST',
+      success: function(res) {
+        if (res.statusCode == 200 && res.data.code == 0) {
+          var clUserInfo = res.data.dataMap.clBaseInfo.clUserInfo;
+          var clCarInfo = res.data.dataMap.clBaseInfo.clCarInfo;
+          var carList = res.data.dataMap.carList;
+          for (var index in carList) {
+            carlist[index] = carList[index].fast_dfs_path
+          }
+          var idCardList = res.data.dataMap.idCardList;
+          for (var index in idCardList) {
+            imglist[index] = idCardList[index].fast_dfs_path
+          }
+          that.setData({
+            clCarInfo: clCarInfo,
+            clUserInfo: clUserInfo,
+            imglist: imglist,
+            carlist: carlist,
+            userInfo: app.globalData.userInfo,
+            biz_order_no: bizOrderNo
+          })
+        }
+      },
+      fail: function() {
+        console.log("获取后台失败！");
+      }
+    })
   },
 
   /**
