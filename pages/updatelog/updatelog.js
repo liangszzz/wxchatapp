82c60a9c935abd8ffd2bee78235a4fe3499b3df4
Walkerontheway <1521481790@qq.com>
2018-10-25 14:32:18 +0800

    更新日志

--- pages/updatelog/updatelog.js
+++ pages/updatelog/updatelog.js
@@ -1,17 +1,43 @@
 // pages/updatelog/updatelog.js
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    list:{}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var that = this;
+    wx.request({
+      url: app.globalData.http_url_head + 'updateLog/query',
+      header: {
+        token: app.globalData.userInfo.token
+      },
+      method: 'POST',
+      success: function (res) {
+        if (res.statusCode == 200 && res.data.code == 0) {
+          var list = res.data.data;
+          that.setData({
+            list: list
+          })
+        } else {
+          wx.showToast({
+            title: '获取消息列表失败',
+            icon: 'none',
+            duration: 2000 //持续的时间
+          })
+        }
+      },
+      fail: function (e) {
+        console.log("请求后台异常！")
+      }
+    })
 
   },
 
