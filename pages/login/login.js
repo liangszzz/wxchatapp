b7449b103ca8eaa172b7aaec6b59e3534122fd45
yiyan1992 <liang-shan@outlook.com>
2018-11-06 17:59:08 +0800

    register

--- pages/login/login.js
+++ pages/login/login.js
@@ -7,7 +7,9 @@ Page({
         phoneCheck: false,
         invitation_code: "",
         invitationCheck: false,
-        btnRegistered: true
+        btnRegistered: true,
+        btnSendSms: true,
+        btnSendSmsMsg: "获取验证码"
     },
     onLoad: function() {
 
@@ -18,11 +20,13 @@ Page({
         })
         if (this.data.phone.length == 11) {
             this.setData({
-                phoneCheck: true
+                phoneCheck: true,
+                btnSendSms: false
             })
         } else {
             this.setData({
-                phoneCheck: false
+                phoneCheck: false,
+                btnSendSms: true
             })
         }
         if (this.data.phoneCheck && this.data.invitationCheck) {
@@ -58,6 +62,46 @@ Page({
             })
         }
     },
+    sendSms: function(e) {
+        this.setData({
+            btnSendSms: true
+        });
+        var that = this;
+        wx.request({
+            url: app.globalData.http_url_head + 'login/wxSendSms/' + this.data.phone,
+            success: function(e) {
+                console.log("###")
+                console.log(e)
+                if (e.data.code == 0) {
+                    let btnSendSmsMsg = 60;
+                    let interval = setInterval(function() {
+                        that.setData({
+                            btnSendSmsMsg: btnSendSmsMsg-- + "s"
+                        });
+                        if (btnSendSmsMsg <= 0) {
+                            that.setData({
+                                btnSendSmsMsg: "获取验证码",
+                                btnSendSms: false
+                            });
+                            clearInterval(interval)
+                        }
+                    }, 1000)
+                }
+            },
+            fail: function(e) {
+                wx.showModal({
+                    title: '发送失败',
+                    content: "发送验证码失败!",
+                    showCancel: false,
+                    confirmText: "确定"
+                })
+                that.setData({
+                    btnSendSms: true
+                })
+            }
+        })
+
+    },
     getUserInfo: function(e) {
         var data = e.detail.userInfo
         data.phone = this.data.phone
@@ -82,7 +126,7 @@ Page({
                                     success: res => {
                                         console.log(res)
                                     },
-                                    fail:res=>{
+                                    fail: res => {
                                         console.log(res)
                                     }
                                 })
