79c65c91b227ad837c9e929d0d6ee852fbcaf6ff
Walkerontheway <1521481790@qq.com>
2018-11-07 16:40:01 +0800

    车辆基本信息

--- pages/carinfo/carinfo.js
+++ pages/carinfo/carinfo.js
@@ -6,7 +6,12 @@ Page({
    * 页面的初始数据
    */
   data: {
+    clRiskInfo: {},
     clCarInfo: {},
+    carlist: [],
+    dengjiList: [],
+    xingshiList: [],
+    formType: '', // 1：订单页进入 2：我的页面进入
   },
 
   /**
@@ -14,28 +19,51 @@ Page({
    */
   onLoad: function(options) {
     var that = this;
+    var idcard = '';
+    var biz_order_no = '';
+    if (options.biz_order_no != '' && options.biz_order_no != null) { //从列表页进入
+      biz_order_no = options.biz_order_no;
+    } else { //从我的页面进入
+      idcard = options.idcard;
+    }
+    var carlist = that.data.carlist;
+    var dengjiList = that.data.dengjiList;
+    var xingshiList = that.data.xingshiList;
     that.initValidate();
-    var idCard = app.globalData.userInfo.idcard;
     //请求后台获取相关信息
     wx.request({
-      url: app.globalData.http_url_head + "car/query/" + idCard,
+      url: app.globalData.http_url_head + "car/query",
       header: {
         token: app.globalData.userInfo.token
       },
+      data: {
+        biz_order_no: biz_order_no,
+        idcard: idcard
+      },
       method: 'POST',
       success: function(res) {
+        console.log(res);
         if (res.statusCode == 200 && res.data.code == 0) {
+          var clRiskInfo = res.data.dataMap.clRiskInfo; //风控信息，用于获取车辆估值
           var clCarInfo = res.data.dataMap.clCarInfo; //车辆基本信息
-          console.log(clCarInfo);
           var carList = res.data.dataMap.carList; //车辆照片
-          console.log(carList);
+          for (var index in carList) {
+            carlist[index] = carList[index].fast_dfs_path
+          }
           var cardriveListList = res.data.dataMap.cardriveListList; //行驶证
-          console.log(cardriveListList);
+          for (var index in cardriveListList) {
+            xingshiList[index] = cardriveListList[index].fast_dfs_path
+          }
           var registerList = res.data.dataMap.registerList //机动车登记证书
-          console.log(registerList);
-
+          for (var index in registerList) {
+            dengjiList[index] = registerList[index].fast_dfs_path
+          }
           that.setData({
+            clRiskInfo: clRiskInfo,
             clCarInfo: clCarInfo,
+            carlist: carlist,
+            xingshiList: xingshiList,
+            dengjiList: dengjiList
           })
         }
       },
@@ -46,51 +74,120 @@ Page({
   },
 
   /**
-   * 图片预览
+   * 车辆照片预览
    */
-  previewImage: function (e) {
+  previewImageCar: function(e) {
     var currentUrl = e.target.dataset.src;
-    if (currentUrl == '') {//缺少图片
-      wx.chooseImage({
-        count: 1,//一次只允许一张
-        sizeType: ['original', 'compressed'],  //可选择原图或缩略图
-        sourceType: ['album', 'camera'], //访问相册、相机
-        success: function (res) {
-          var tempFilePaths = res.tempFilePaths;
-          //图片上传
-          wx.uploadFile({
-            url: this.globalData.http_url_head + 'attachmentInfo/uploadFile',
-            filePath: tempFilePaths[0],
-            name: 'file',
-            header: {
-              "Content-Type": "multipart/form-data",
-              token: app.globalData.userInfo.token
-            },
-            formData: { //
-
-            },
-            success: function (res) {
-              console.log(res);
-            },
-            fail: function () {
-              console.log("身份证照上传失败！");
-            }
-          })
-        },
-      })
+    if (currentUrl == '' || currentUrl == null) { //缺少图片
+      this.chooseImg(7, );
     } else {
       wx.previewImage({
         current: currentUrl, // 当前显示图片的http链接
-        urls: this.data.imglist // 需要预览的图片http链接列表
+        urls: this.data.carlist // 需要预览的图片http链接列表
       })
     }
+  },
+
+  /**
+   * 抵押登记证预览
+   */
+  previewImageDeng: function(e) {
+    var currentUrl = e.target.dataset.src;
+    if (currentUrl == '' || currentUrl == null) { //缺少图片
+      this.chooseImg(4);
+    } else {
+      wx.previewImage({
+        current: currentUrl, // 当前显示图片的http链接
+        urls: this.data.dengjiList // 需要预览的图片http链接列表
+      })
+    }
+  },
+
+  /**
+   * 行驶证预览
+   */
+  previewImageXing: function(e) {
+    var currentUrl = e.target.dataset.src;
+    if (currentUrl == '' || currentUrl == null) { //缺少图片
+      this.chooseImg(14);
+    } else {
+      wx.previewImage({
+        current: currentUrl, // 当前显示图片的http链接
+        urls: this.data.xingshiList // 需要预览的图片http链接列表
+      })
+    }
+  },
+
+  /**
+   * 缺少图片上传图片公用方法
+   */
+  chooseImg: function(file_type) {
+    var that = this;
+    var carlist = that.data.carlist;
+    var dengjiList = that.data.dengjiList;
+    var xingshiList = that.data.xingshiList;
+    wx.chooseImage({
+      count: 1, //一次只允许一张
+      sizeType: ['original', 'compressed'], //可选择原图或缩略图
+      sourceType: ['album', 'camera'], //访问相册、相机
+      success: function(res) {
+        var tempFilePaths = res.tempFilePaths;
+        //图片上传
+        wx.uploadFile({
+          url: app.globalData.http_url_head + 'attachmentInfo/uploadFile',
+          filePath: tempFilePaths[0],
+          name: 'file',
+          header: {
+            "Content-Type": "multipart/form-data",
+            token: app.globalData.userInfo.token
+          },
+          formData: {
+            file_type: file_type,
+            biz_order_no: that.data.clCarInfo.biz_order_no
+          },
+          success: function(res) {
+            let data = JSON.parse(res.data);
+            if (res.statusCode == 200 && data.code == 0) {
+              if (file_type == 7) { //车辆照片
+                carlist[carlist.length] = data.entity.file_path;
+                that.setData({
+                  carlist: carlist
+                })
+              } else if (file_type == 4) { //抵押登记照片
+                dengjiList[dengjiList.length] = data.entity.file_path;
+                that.setData({
+                  dengjiList: dengjiList
+                })
+              } else {
+                xingshiList[xingshiList.length] = data.entity.file_path;
+                that.setData({
+                  xingshiList: xingshiList
+                })
+              }
+            }
+          },
+          fail: function() {
+            console.log("身份证照上传失败！");
+          }
+        })
+      },
+    })
   },
 
   /**
    * 下一步
    */
   formSubmit: function(e) {
-    var openId = this.data.userInfo.openId;
+    //先检查图片是否存在
+    if (this.data.carlist.length < 1 || this.data.dengjiList.length < 1 || this.data.xingshiList.length < 2){
+      wx.showToast({
+        title: '缺少图片信息，请上传图片',
+        icon: 'none',
+        duration: 2000 //持续的时间
+      })
+      return false
+    }
+    var openId = app.globalData.userInfo.openId;
     var formId = e.detail.formId;
     let data = e.detail.value
     console.log(data)
@@ -100,103 +197,49 @@ Page({
       console.log(error);
       return false
     }
+    var that = this;
+    //后台保存数据
+    wx.request({
+      url: app.globalData.http_url_head + 'car/save',
+      header: {
+        token: app.globalData.userInfo.token
+      },
+      data: {
+        data: data
+      },
+      method: "post",
+      success: function(res) {
+        console.log(res);
+        if(res.statusCode == 200 && res.data.code == 0){
+         wx.navigateTo({
+           url: '../sign/sign?biz_order_no=' + that.data.clCarInfo.biz_order_no,
+         })
+        }else{
+          wx.showToast({
+            title: res.data.msg,
+            icon: 'none',
+            duration: 2000 //持续的时间
+          })
+        }
+      },
+      fail: function() {
+        console.log("数据保存失败")
+      }
+    })
   },
 
   initValidate() {
     // 验证字段的规则
     const rules = {
-      gender: {
+      car_no: {
         required: true,
-      },
-      assistance: {
-        required: true,
-        assistance: true,
-      },
-      email: {
-        required: true,
-        email: true,
-      },
-      tel: {
-        required: true,
-        tel: true,
-      },
-      idcard: {
-        required: true,
-        idcard: true,
-      },
-      password: {
-        required: true,
-        minlength: 6,
-        maxlength: 15,
-      },
-      confirmPassword: {
-        required: true,
-        minlength: 6,
-        maxlength: 15,
-        equalTo: 'password',
-      },
-      countryIndex: {
-        required: true,
-      },
-      slider: {
-        required: true,
-        min: 40,
-        max: 80,
-      },
-      agree: {
-        required: true,
-      },
-      textarea: {
-        required: true,
-        contains: '自愿',
       },
     }
 
     // 验证字段的提示信息，若不传则调用默认的信息
     const messages = {
-      gender: {
-        required: '请选择性别',
-      },
-      assistance: {
-        required: '请勾选1-2个敲码助手',
-      },
-      email: {
-        required: '请输入邮箱',
-        email: '请输入正确的邮箱',
-      },
-      tel: {
-        required: '请输入手机号',
-        tel: '请输入正确的手机号',
-      },
-      idcard: {
-        required: '请输入身份证号码',
-        idcard: '请输入正确的身份证号码',
-      },
-      password: {
-        required: '请输入新密码',
-        minlength: '密码长度不少于6位',
-        maxlength: '密码长度不多于15位',
-      },
-      confirmPassword: {
-        required: '请输入确认密码',
-        minlength: '密码长度不少于6位',
-        maxlength: '密码长度不多于15位',
-        equalTo: '确认密码和新密码保持一致',
-      },
-      countryIndex: {
-        required: '请选择国家/地区',
-      },
-      slider: {
-        required: '请选择年龄',
-        min: '年龄不小于18',
-        max: '年龄不大于60',
-      },
-      agree: {
-        required: '请同意我们的声明',
-      },
-      textarea: {
-        required: '请输入文本',
-        contains: '请输入文本（必须含有自愿两字）',
+      car_no: {
+        required: '车牌号码不能为空',
       },
     }
     // 创建实例对象
