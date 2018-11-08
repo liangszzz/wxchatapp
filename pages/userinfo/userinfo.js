3e0e79c50221fc870825a638317ebcea2870e324
Walkerontheway <1521481790@qq.com>
2018-11-08 17:54:59 +0800

    个人资料

--- pages/userinfo/userinfo.js
+++ pages/userinfo/userinfo.js
@@ -11,6 +11,20 @@ Page({
     clUserInfo: {},
     userInfo:{},
     fromType: '', // 1：订单页进入 2：我的页面进入
+    helthIndex: 0,
+    helthArray: [],
+    idIndex: 0,
+    idArray: [],
+    eduIndex: 0,
+    eduArray: [],
+    clientIndex: 0,
+    clientArray: [],
+    bankIndex: 0,
+    bankArray: [],
+    marriageIndex: 0,
+    marriageArray: [],
+    relationshipArray:[],
+    relationIndexArray: []
   },
 
   /**
@@ -30,6 +44,12 @@ Page({
     }
     that.initValidate()
     var imglist = that.data.imglist;
+    var helthIndex = that.data.helthIndex;
+    var idIndex = that.data.idIndex;
+    var eduIndex = that.data.eduIndex;
+    var clientIndex = that.data.clientIndex;
+    var bankIndex = that.data.bankIndex;
+    var marriageIndex = that.data.marriageIndex;
     //请求后台获取相关信息
     wx.request({
       url: app.globalData.http_url_head + "user/query",
@@ -42,21 +62,80 @@ Page({
         idcard: idcard
       },
       success: function(res) {
+        console.log(res);
         if (res.statusCode == 200 && res.data.code == 0) {
-          var clUserInfo = res.data.dataMap.clUserInfo;
-          console.log(clUserInfo);
-          var clContactInfoList = res.data.dataMap.clContactInfoList;
-          console.log(clContactInfoList);
-          var clAttachmentInfoList = res.data.dataMap.clAttachmentInfoList;
-          console.log(clAttachmentInfoList);
+          var clUserInfo = res.data.dataMap.clUserInfo; //用户基本信息
+          var clContactInfoList = res.data.dataMap.clContactInfoList; //联系人信息
+          var clAttachmentInfoList = res.data.dataMap.clAttachmentInfoList; //附件信息
           for (var index in clAttachmentInfoList) {
             imglist[index] = clAttachmentInfoList[index].fast_dfs_path
           }
+          var helthArray = res.data.dataMap.health; //身体状况
+          for (var index in helthArray) {
+            if (helthArray[index].value == clUserInfo.health_status) {
+              helthIndex = index;
+            }
+          }
+          var idArray = res.data.dataMap.identity_type; //身份类型
+          for (var index in idArray) {
+            if (idArray[index].value == clUserInfo.identity_type) {
+              idIndex = index;
+            }
+          }
+          var eduArray = res.data.dataMap.degreeS; //最高学历
+          for (var index in eduArray) {
+            if (eduArray[index].value == clUserInfo.degree) {
+              eduIndex = index;
+            }
+          }
+          var clientArray = res.data.dataMap.customerInfo; //客户职业信息
+          for (var index in clientArray) {
+            if (clientArray[index].value == clUserInfo.customer_professional_info) {
+              clientIndex = index;
+            }
+          }
+          var bankArray = res.data.dataMap.bankCards; //银行卡类型
+          for (var index in bankArray) {
+            if (bankArray[index].value == clUserInfo.bank_card_type) {
+              bankIndex = index;
+            }
+          }
+          var marriageArray = res.data.dataMap.marital_status;//婚姻状况
+          for (var index in idArray) {
+            if (marriageArray[index].value == clUserInfo.marital_status) {
+              marriageIndex = index;
+            }
+          }
+          var relationIndexArray = that.data.relationIndexArray;
+          var relationshipArray = res.data.dataMap.relationShip; //社会关系
+          for (var i in clContactInfoList) {
+            var relation = clContactInfoList[i].contact_relationship;
+            for (var index in relationshipArray){
+              if (relation == relationshipArray[index].value){
+                relationIndexArray[i] = index
+              }
+            }
+          }
+          console.log(relationIndexArray);
           that.setData({
             clUserInfo: clUserInfo,
             imglist: imglist,
             clContactInfoList: clContactInfoList,
-            fromType: fromType
+            fromType: fromType,
+            helthArray: helthArray,
+            idArray: idArray,
+            eduArray: eduArray,
+            clientArray: clientArray,
+            bankArray: bankArray,
+            marriageArray: marriageArray,
+            helthIndex: helthIndex,
+            idIndex: idIndex,
+            clientIndex, clientIndex,
+            bankIndex: bankIndex,
+            eduIndex: eduIndex,
+            marriageIndex: marriageIndex,
+            relationshipArray: relationshipArray,
+            relationIndexArray: relationIndexArray
           })
         }
       },
@@ -134,10 +213,45 @@ Page({
     }
     // 传入表单数据，调用验证方法
     if (!this.WxValidate.checkForm(data)) {
-      const error = this.WxValidate.errorList[0]
-      console.log(error);
+      const error = this.WxValidate.errorList[0];
+      wx.showToast({
+        title: error.msg,
+        icon: 'none',
+        duration: 2000 //持续的时间
+      })
       return false
     }
+   
+    var health_status = this.data.helthArray[data.health_status].value;
+    var identity_type = this.data.idArray[data.identity_type].value;
+    var degree = this.data.eduArray[data.degree].value;
+    var customer_professional_info = this.data.clientArray[data.customer_professional_info].value;
+    var bank_card_type = this.data.bankArray[data.bank_card_type].value;
+    var marital_status = this.data.marriageArray[data.marital_status].value;
+
+    data.health_status = health_status;
+    data.identity_type = identity_type;
+    data.degree = degree;
+    data.customer_professional_info = customer_professional_info;
+    data.bank_card_type = bank_card_type;
+    data.marital_status = marital_status;
+    
+    var clContactInfoList = this.data.clContactInfoList;
+    var contactArray = [];
+    for (var index in clContactInfoList){
+      var item = new Object();
+      var name = 'contact_name_' + index;
+      var phone = 'contact_phone_' + index;
+      var relationShip = 'contact_relationship_'+index;
+      var contact_name = data[name];
+      var contact_phone = data[phone];
+      var contact_relationship = this.data.relationshipArray[data[relationShip]].value;
+      item.contact_name = contact_name;
+      item.contact_phone = contact_phone;
+      item.contact_relationship = contact_relationship;
+      contactArray[index] = item;
+    }
+    data.contactArray = contactArray;
     //后台保存数据
     wx.request({
       url: app.globalData.http_url_head+'user/save',
@@ -167,15 +281,115 @@ Page({
       idcard_address: {
         required: true,
       },
+      bank_account: {
+        required: true,
+      },
+      personal_income: {
+        required: true,
+      },
+      bank_name: {
+        required: true,
+      },
+      certificate_expiry_date: {
+        required: true,
+      },
+      company_name: {
+        required: true,
+      },
+      company_phone_no: {
+        required: true,
+      },
+      reserve_phone_no: {
+        required: true,
+      },
+      residential_address: {
+        required: true,
+      },
+      wechat: {
+        required: true,
+      },
     }
 
     // 验证字段的提示信息，若不传则调用默认的信息
     const messages = {
       idcard_address: {
-        required: '身份证居住地址不能为空',
+        required: "身份证地址不能为空",
+      },
+      bank_account: {
+        required: "银行卡号不能为空",
+      },
+      bank_name: {
+        required: "开户行名称不能为空",
+      },
+      certificate_expiry_date: {
+        required: "证件到期时间不能为空",
+      },
+      company_name: {
+        required: "公司名称不能为空",
+      },
+      company_phone_no: {
+        required: "公司电话不能为空",
+      },
+      reserve_phone_no: {
+        required: "预留手机号码不能为空",
+      },
+      residential_address: {
+        required: "现居住地址不能为空",
+      },
+      wechat: {
+        required: "微信号不能为空",
+      },
+      personal_income: {
+        required: "个人年总收入不能为空",
       },
     }
     // 创建实例对象
     this.WxValidate = new WxValidate(rules, messages)
   },
+
+
+/**
+ * 所有选择监听
+ */
+  bindHelthChange:function(e){
+    this.setData({
+      helthIndex: e.detail.value
+    })
+  },
+  bindidChange: function (e) {
+    this.setData({
+      idIndex: e.detail.value
+    })
+  },
+  bindeduChange: function (e) {
+    this.setData({
+      eduIndex: e.detail.value
+    })
+  },
+  bindclientChange: function (e) {
+    this.setData({
+      clientIndex: e.detail.value
+    })
+  },
+  bindbankChange: function (e) {
+    this.setData({
+      bankIndex: e.detail.value
+    })
+  },
+  bindmarriageChange: function (e) {
+    this.setData({
+      marriageIndex: e.detail.value
+    })
+  },
+
+  bindshipChange: function (e) {
+    console.log(e.target.dataset.id); //relationIndexArray下标
+    console.log(e.detail.value); //relationShip下标
+    var relationIndexArray = this.data.relationIndexArray;
+    relationIndexArray[e.target.dataset.id] = e.detail.value;
+    this.setData({
+      relationIndexArray: relationIndexArray
+    })
+  },
+
 })
