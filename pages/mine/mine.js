d9de6271889659668207b58aeaab9c27c2468ccb
Walkerontheway <1521481790@qq.com>
2018-11-12 17:14:15 +0800

    我的页面添加下拉刷新

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -10,7 +10,7 @@ Page({
     userInfo: {},
     orderStatus: '',
     biz_order_no: '',
-    loanAmount:''
+    loanAmount: ''
   },
 
   /**
@@ -61,7 +61,7 @@ Page({
   /**
    * 我的车辆信息
    */
-  toCarInfo:function() {
+  toCarInfo: function() {
     wx.navigateTo({
       url: '../carinfo/carinfo?biz_order_no=' + this.data.biz_order_no + '&fromType=2&orderStatus=' + this.data.orderStatus,
     })
@@ -72,15 +72,23 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
+    
+    this.findUserInfo();
+    this.setData({
+      userInfo: app.globalData.userInfo
+    })
+  },
+
+  findUserInfo: function(){
     var that = this;
     //获取该用户最近的订单
     wx.request({
-      url: app.globalData.http_url_head + 'user/getRecentOrder/'+app.globalData.userInfo.idcard,
+      url: app.globalData.http_url_head + 'user/getRecentOrder/' + app.globalData.userInfo.idcard,
       method: 'post',
       header: {
         token: app.globalData.userInfo.token
       },
-      success: function(res) {
+      success: function (res) {
         if (res.statusCode == 200 && res.data.code == 0) {
           var bizOrderNo = res.data.entity.biz_order_no;
           var apply_amount = res.data.entity.apply_amount;
@@ -98,14 +106,22 @@ Page({
           })
         }
       },
-      fail: function() {
+      fail: function () {
         console.log("获取后台数据失败")
       }
     })
-    that.setData({
-      userInfo: app.globalData.userInfo
-    })
   },
+
+  /**
+   * 下拉刷新
+   */
+  onPullDownRefresh: function() {
+    wx.showNavigationBarLoading() //在标题栏中显示加载
+    this.findUserInfo();
+    wx.stopPullDownRefresh();
+    wx.hideNavigationBarLoading();
+
+  }
 
 
 
