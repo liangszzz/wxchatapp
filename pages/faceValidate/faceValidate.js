29ccf1b33ba4fc6d41e722b346022c66def62ddf
Walkerontheway <1521481790@qq.com>
2018-12-20 14:47:16 +0800

    人脸验证

--- pages/faceValidate/faceValidate.js
+++ pages/faceValidate/faceValidate.js
@@ -6,8 +6,8 @@ Page({
    */
   data: {
     biz_order_no: '',
-    imageData:'',
-    isValidate:false
+    imageData: '',
+    isValidate: false
   },
 
   /**
@@ -24,12 +24,11 @@ Page({
     let that = this;
     let videoBase64String = ''
     let biz_order_no = this.data.biz_order_no
-    console.log(biz_order_no)
     wx.chooseImage({
       count: 1, //一次只允许一张
       sizeType: ['original', 'compressed'], //可选择原图或缩略图
       sourceType: ['album', 'camera'], //访问相册、相机
-      success: function (res) {
+      success: function(res) {
         let tempFilePaths = res.tempFilePaths;
         wx.getFileSystemManager().readFile({
           filePath: res.tempFilePaths[0], //选择图片返回的相对路径
@@ -41,7 +40,7 @@ Page({
             })
 
             wx.request({
-              url: app.globalData.http_url_head + "face/validate",
+              url: app.globalData.http_url_head + "face/verify",
               method: 'POST',
               header: {
                 token: app.globalData.userInfo.token
@@ -50,23 +49,47 @@ Page({
                 bizOrderNo: biz_order_no,
                 videoBase64String: videoBase64String
               },
-              success: function (res) {
-                console.log(res)
-                if(res.statusCode == 200 && res.data.code == 0){
+              success: function(res) {
+                if (res.statusCode == 200 && res.data.code == 0) {
                   wx.showToast({
-                    title: '身份证和脸部照片识别成功',
+                    title: '人脸验证成功',
+                  })
+                  that.setData({
+                    isValidate: true
+                  })
+                }else{
+                  wx.showToast({
+                    title: '人脸验证失败，请点击照片重新上传',
+                    icon: 'none',
+                    duration: 2000
                   })
                 }
               },
-              fail: function () {
-
+              fail: function() {
+                console.log("服务器异常")
               }
             })
           }
         })
 
-        
+
       },
+    })
+  },
+
+
+  toNext: function() {
+    if (this.data.isValidate == false) {
+      wx.showToast({
+        title: '请先完成人脸验证',
+        icon: 'none',
+        duration: 2000
+      })
+      return false;
+    }
+
+    wx.navigateTo({
+      url: '../sign/sign?biz_order_no=' + this.data.biz_order_no + '&page_type=1&channel_type=1'
     })
   }
 
